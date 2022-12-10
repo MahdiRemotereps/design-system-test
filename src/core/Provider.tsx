b2457eb45b695e593f3502f7168968028e10ec8c
RemoteReps <Mahdi@Mahdis-MacBook-Pro.local>
2022-12-10 19:43:13 +0330

    remote

--- src/core/Provider.tsx
+++ src/core/Provider.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { PropsWithChildren } from 'react';
-import '../styles/globals.css';
 
 const Provider: React.FC<PropsWithChildren> = (props) => {
   return <>{props.children}</>;
